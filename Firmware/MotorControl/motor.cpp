8f1543df792858f2fb2d426cc616779d03825509
Troy Benjegerdes <troy@7el.us>
2021-01-29 16:55:48 -0600

    Actually report ESTOP_ACTIVATED

--- Firmware/MotorControl/motor.cpp
+++ Firmware/MotorControl/motor.cpp
@@ -176,7 +176,7 @@ bool Motor::update_thermal_limits() {
 
 bool Motor::do_checks() {
     if (!check_estop_active()) {
-        set_error(ERROR_NOT_IMPLEMENTED_MOTOR_TYPE);
+        set_error(ERROR_ESTOP_ACTIVATED);
         return false;
     }
     if (!check_DRV_fault()) {
